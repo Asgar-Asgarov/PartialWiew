78bcfdc70f7c94ec7ad3d1350359a4a97cf14820
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-02-22 07:44:00 +0400

    feature:MVC project is created and some useless files are removed

---
+++ FronToBack/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FronToBack.Models;
+
+namespace FronToBack.Controllers;
+
+public class HomeController : Controller
+{
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+  
+   
+}
