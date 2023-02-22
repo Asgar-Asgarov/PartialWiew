8160af4f401673cba686f4e5eabc16d69d1d7215
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-02-22 09:02:42 +0400

    refactor:connection string is added,feature:congfig method is added'

--- FronToBack/DAL/AppDbContext.cs
+++ FronToBack/DAL/AppDbContext.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using FronToBack.Models;
 namespace FronToBack.DAL;
 class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions options) : base(options)
     {
     }
+    public DbSet<Student> Students{get;set;}
 }
--- FronToBack/Program.cs
+++ FronToBack/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using FronToBack.DAL;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var _config=builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options=>{
+options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+// options.UseSqlServer(_config["ConnectionStrings:DefaultConnection"]);  //2nd way .This way is not good,because writing strings in this way aren`t liked.
+
+}
+
+);
 
 var app = builder.Build();
 
